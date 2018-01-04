8383f6e36b26eee7cb0cd72d0d55920672c4641d
Jacky Wu <jacky.wu@soundbus.cn>
2018-01-04 19:02:46 +0800

    Use gin to deal with http requests.

--- media-player/player.go
+++ media-player/player.go
@@ -10,6 +10,7 @@ import (
 	"time"
 
 	"github.com/minio/minio-go"
+	"github.com/gin-gonic/gin"
 )
 
 var (
@@ -107,20 +108,88 @@ func main() {
 		minioClient: minioClient,
 	}
 
-	// Handler to serve the index page.
-	http.Handle("/", http.FileServer(assetFS()))
+	var enableGin = true
+	if (enableGin) {
+		r := gin.Default()
+		// Handler to serve the index page.
+		r.Static("/player", "web")
 
-	// End point for list object operations.
-	// Called when player in the front end is initialized.
-	http.HandleFunc("/list/v1", mediaPlayer.ListObjectsHandler)
+		// End point for list object operations.
+		// Called when player in the front end is initialized.
+		r.GET("/list/v1", mediaPlayer.ListObjectsHandler2)
 
-	// Given point which receives the object name and returns presigned URL in the response.
-	http.HandleFunc("/getpresign/v1", mediaPlayer.GetPresignedURLHandler)
+		// Given point which receives the object name and returns presigned URL in the response.
+		r.GET("/getpresign/v1", mediaPlayer.GetPresignedURLHandler2)
 
-	log.Println("Starting media player, please visit your browser at http://localhost:8080")
+		log.Println("Starting media player, please visit your browser at http://localhost:8080/player/index.html")
 
-	// Port is defaulted to "8080" no need to change this.
-	http.ListenAndServe(":8080", nil)
+		// Port is defaulted to "8080" no need to change this.
+		r.Run(":8080")
+	} else {
+		// Handler to serve the index page.
+		http.Handle("/", http.FileServer(assetFS()))
+
+		// End point for list object operations.
+		// Called when player in the front end is initialized.
+		http.HandleFunc("/list/v1", mediaPlayer.ListObjectsHandler)
+
+
+		// Given point which receives the object name and returns presigned URL in the response.
+		http.HandleFunc("/getpresign/v1", mediaPlayer.GetPresignedURLHandler)
+
+		log.Println("Starting media player, please visit your browser at http://localhost:8080")
+
+		// Port is defaulted to "8080" no need to change this.
+		http.ListenAndServe(":8080", nil)
+	}
+}
+
+func (api mediaHandlers) ListObjectsHandler2(c *gin.Context) {
+	// Create a done channel to control 'ListObjects' go routine.
+	doneCh := make(chan struct{})
+
+	// Indicate to our routine to exit cleanly upon return.
+	defer close(doneCh)
+
+	var playListEntries []mediaPlayList
+
+	// Tracks if first object presigned.
+	var firstObjectPresigned bool
+
+	// Set recursive to list all objects.
+	var isRecursive = true
+
+	// List all objects from a bucket-name with a matching prefix.
+	for objectInfo := range api.minioClient.ListObjects(*bucketName, "", isRecursive, doneCh) {
+		if objectInfo.Err != nil {
+			c.String(http.StatusInternalServerError, objectInfo.Err.Error())
+			return
+		}
+		objectName := objectInfo.Key // object name.
+		playListEntry := mediaPlayList{
+			Key: objectName,
+		}
+		if !firstObjectPresigned {
+			// Generating presigned url for the first object in the list.
+			// presigned URL will be generated on the fly for the
+			// other objects when they are played.
+			expirySecs := 1000 * time.Second // 1000 seconds.
+			presignedURL, err := api.minioClient.PresignedGetObject(*bucketName, objectName, expirySecs, nil)
+			if err != nil {
+				c.String(http.StatusInternalServerError, err.Error())
+				return
+			}
+			playListEntry.URL = presignedURL.String()
+		}
+		playListEntries = append(playListEntries, playListEntry)
+	}
+	playListEntriesJSON, err := json.Marshal(playListEntries)
+	if err != nil {
+		c.String(http.StatusInternalServerError, err.Error())
+		return
+	}
+	// Successfully wrote play list in json.
+	c.JSON(http.StatusOK, string(playListEntriesJSON))
 }
 
 // ListObjectsHandler - handler for ListsObjects from the Object Storage server and bucket configured above.
@@ -173,10 +242,28 @@ func (api mediaHandlers) ListObjectsHandler(w http.ResponseWriter, r *http.Reque
 }
 
 // GetPresignedURLHandler - generates presigned access URL for an object.
+func (api mediaHandlers) GetPresignedURLHandler2(c *gin.Context) {
+	// The object for which the presigned URL has to be generated is sent as a query
+	// parameter from the client.
+	objectName := c.Query("objname")
+	if objectName == "" {
+		c.String(http.StatusBadRequest, "No object name set, invalid request.")
+		return
+	}
+	presignedURL, err := api.minioClient.PresignedGetObject(*bucketName, objectName, 1000*time.Second, nil)
+	if err != nil {
+		c.String(http.StatusInternalServerError, err.Error())
+		return
+	}
+	c.String(http.StatusOK, presignedURL.String())
+}
+
+
+// GetPresignedURLHandler - generates presigned access URL for an object.
 func (api mediaHandlers) GetPresignedURLHandler(w http.ResponseWriter, r *http.Request) {
 	// The object for which the presigned URL has to be generated is sent as a query
 	// parameter from the client.
-	objectName := r.URL.Query().Get("objName")
+	objectName := r.URL.Query().Get("objname")
 	if objectName == "" {
 		http.Error(w, "No object name set, invalid request.", http.StatusBadRequest)
 		return
